2ba04e4771be340c558b0efbfa715762623b62b6
Lavanya Jose <lavanya.jose@gmail.com>
2020-09-11 17:53:17 -0700

    Replace deprecated urllib.FancyURLopener

--- get.py
+++ get.py
@@ -4,14 +4,14 @@
 import sys
 import os
 import glob
-import urllib
+import urllib.request
 import json
 
 path = sys.argv[1]
 #path = "20130524_061236_335/"
 #for infile in glob.glob(os.path.join(path, '*gps.txt')):
 
-f = open(path, 'r')
+f = urllib.request.urlopen(path, 'r')
 # read line from gps text file
 
 TIME_GPS=f.readline()
@@ -29,9 +29,8 @@ YYYY = YYYYMMDD[:4]
 MM = YYYYMMDD[4:6]
 DD = YYYYMMDD[6:8]
 
-opener = urllib.FancyURLopener({})
 APIkey = "4d53ca5971e81aa7e60f1bfecf462f01"
 
-f = opener.open("https://api.forecast.io/forecast/%s/%s,%s,%s-%s-%sT%s:00:00-0400" % (APIkey,coords[1][1:-1],coords[0][1:-1],YYYY,MM,DD,HH))
+f = urllib.request.urlopen("https://api.forecast.io/forecast/%s/%s,%s,%s-%s-%sT%s:00:00-0400" % (APIkey,coords[1][1:-1],coords[0][1:-1],YYYY,MM,DD,HH))
 #print(("https://api.forecast.io/forecast/%s/%s,%s,%s-%s-%sT%s:00:00-0400" % (APIkey,coords[1][1:-1],coords[0][1:-1],YYYY,MM,DD,HH)))
-print f.read()
+print(f.read())
